7812cf261a56dc142b8e5251cb2b07e876c75ab5
Saman Hoseini <saman2000hoseini@gmail.com>
2020-10-24 05:54:39 -0400

    feat: add testcase input/output addition

--- main.go
+++ main.go
@@ -18,29 +18,38 @@ func New() TestCase {
 	}
 }
 
-func (T *TestCase) Write(path string) {
+func (T *TestCase) Write(path string) error {
 	err := os.Mkdir(path, 0755)
 	if err != nil {
-		log.Fatalf("cannot create directory: %s", err.Error())
+		return err
 	}
 
 	err = os.Mkdir(path+"/in", 0755)
 	if err != nil {
-		log.Fatalf("cannot create directory: %s", err.Error())
-	}
-
-	err = os.Mkdir(path+"/out", 0755)
-	if err != nil {
-		log.Fatalf("cannot create directory: %s", err.Error())
+		return err
 	}
 
 	for i, input := range *T.in {
 		go export(fmt.Sprintf("%s/in/input%d.txt", path, i+1), input.([]interface{}))
 	}
 
+	err = os.Mkdir(path+"/out", 0755)
+	if err != nil {
+		return err
+	}
+
 	for i, output := range *T.out {
 		go export(fmt.Sprintf("%s/out/output%d.txt", path, i+1), output.([]interface{}))
 	}
+	return nil
+}
+
+func (T TestCase) AddInput(input []interface{}) {
+	*T.in = append(*T.in, input)
+}
+
+func (T TestCase) AddOutput(output []interface{}) {
+	*T.out = append(*T.out, output)
 }
 
 func export(path string, data []interface{}) {
