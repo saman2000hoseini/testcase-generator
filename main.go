233fbcf400a9200b425a3568116713c6c1f31c24
Saman Hoseini <saman2000hoseini@gmail.com>
2020-10-24 05:35:39 -0400

    feat: add testcase struct

--- main.go
+++ main.go
@@ -6,7 +6,19 @@ import (
 	"os"
 )
 
-func Write(in, out []interface{}, path string) {
+type TestCase struct {
+	in  *[]interface{}
+	out *[]interface{}
+}
+
+func New() TestCase {
+	return TestCase{
+		in:  new([]interface{}),
+		out: new([]interface{}),
+	}
+}
+
+func (T *TestCase) Write(path string) {
 	err := os.Mkdir(path, 0755)
 	if err != nil {
 		log.Fatalf("cannot create directory: %s", err.Error())
@@ -22,16 +34,16 @@ func Write(in, out []interface{}, path string) {
 		log.Fatalf("cannot create directory: %s", err.Error())
 	}
 
-	for i := 0; i < len(in); i++ {
-		go export(fmt.Sprintf("%s/in/input%d.txt", path, (i+1)), fmt.Sprintf("%v", in[i]))
+	for i, input := range *T.in {
+		go export(fmt.Sprintf("%s/in/input%d.txt", path, i+1), input.([]interface{}))
 	}
 
-	for i := 0; i < len(out); i++ {
-		go export(fmt.Sprintf("%s/out/output%d.txt", path, (i+1)), fmt.Sprintf("%v", out[i]))
+	for i, output := range *T.out {
+		go export(fmt.Sprintf("%s/out/output%d.txt", path, i+1), output.([]interface{}))
 	}
 }
 
-func export(path string, data string) {
+func export(path string, data []interface{}) {
 	f, err := os.Create(path)
 	if err != nil {
 		log.Fatalf("cannot create directory: %s", err.Error())
@@ -39,8 +51,10 @@ func export(path string, data string) {
 
 	defer f.Close()
 
-	_, err = f.Write([]byte(data))
-	if err != nil {
-		log.Panicf("error while creating %s: %s", path, err.Error())
+	for i := 0; i < len(data); i++ {
+		_, err = f.WriteString(fmt.Sprintf("%v\n", data[i]))
+		if err != nil {
+			log.Panicf("error while creating %s: %s", path, err.Error())
+		}
 	}
 }
